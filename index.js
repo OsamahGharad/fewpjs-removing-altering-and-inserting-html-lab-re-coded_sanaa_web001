cfef6d53e0a2e551f01068512c3ff78273a9be34
Osamah Gharad <osamah.gharred@gmail.com>
2020-08-08 07:52:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Write your code here!
 
+<<<<<<< HEAD
 const main = document.querySelector('main#main');
 main.remove();
 
@@ -7,3 +8,9 @@ const newHeader = document.createElement('h1');
 newHeader.setAttribute('id','victory');
 newHeader.innerHTML= "Osamah is the champion";
 document.body.appendChild(newHeader);
+=======
+let element =document.querySelector("main#main");
+element.Remove();
+
+let newHeader=document.querySelector("h1#victory");
+>>>>>>> e760aa53090f8fcba76349d3aeb7f76f1477a63f
